35d5172c992b84d110665081052bd27de93105a4
Mike Hockerman <mike@hockerman.com>
2015-06-05 10:01:43 -0700

    Moved all non cmd js files to lib folder, added empty config.json, removed possibility for infinite loop by jarvis answering himself.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
-require('./socket/get_slack_ws')(function(err, wsurl) {
-  require('./socket/init')().connect(wsurl)
+var logger = require('log4js').getLogger()
+logger.trace('Starting')
+
+require('./lib/get_slack_ws')(function(err, wsurl) {
+  require('./lib/init_socket')().connect(wsurl)
 })
