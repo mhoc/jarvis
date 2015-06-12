f8be2eefb712056a783108b8ca1cbcb7f4fd7d99
Mike Hockerman <mike@hockerman.com>
2015-06-11 17:19:45 -0700

    Moved envvar check to be a full config check function to warn about unset envvars or config.json keys.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var log = require('tablog')
 log.trace('Starting')
 
 // Check envvars to see if they're all set up
-require('./lib/envvar_check')()
+require('./lib/config_check')()
 
 require('./lib/get_slack_ws')(function(err, wsurl) {
   require('./lib/init_socket')().connect(wsurl)
