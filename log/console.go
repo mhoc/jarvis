df8c3bf0080d9841ac2ab360b9ae51372f96a96a
Mike Hockerman <mike@hockerman.com>
2015-07-30 16:53:17 -0700

    Removed newlines from log printing.

--- log/console.go
+++ log/console.go
@@ -4,6 +4,7 @@ package log
 import (
   "fmt"
   "os"
+  "strings"
   "time"
 )
 
@@ -14,18 +15,22 @@ func Prefix() string {
 }
 
 func Trace(s string, args ...interface{}) {
+  s = strings.Replace(s, "\n", " ", -1)
   fmt.Printf(fmt.Sprintf("%v\n", FormatColor(Prefix() + s, BOLD_GRAY)), args...)
 }
 
 func Info(s string, args ...interface{}) {
+  s = strings.Replace(s, "\n", " ", -1)
   fmt.Printf(fmt.Sprintf("%v%v\n", FormatColor(Prefix(), BOLD_GREEN), s), args...)
 }
 
 func Warn(s string, args ...interface{}) {
+  s = strings.Replace(s, "\n", " ", -1)
   fmt.Printf(fmt.Sprintf("%v%v\n", FormatColor(Prefix(), YELLOW), s), args...)
 }
 
 func Fatal(s string, args ...interface{}) {
+  s = strings.Replace(s, "\n", " ", -1)
   fmt.Printf(fmt.Sprintf("%v\n", FormatColor(Prefix() + s, BOLD_RED)), args...)
   os.Exit(1)
 }
