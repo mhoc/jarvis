440372c8101e5f7aa7217083118c2b56d42d27ce
Mike Hockerman <mike@hockerman.com>
2015-07-31 14:40:56 -0700

    Tied remember zipcode into weather for personalized weather forecasts.

--- commands/weather.go
+++ commands/weather.go
@@ -2,10 +2,10 @@
 package commands
 
 import (
+  "github.com/mhoc/jarvis/data"
   "github.com/mhoc/jarvis/service"
   "github.com/mhoc/jarvis/util"
   "github.com/mhoc/jarvis/ws"
-  "regexp"
 )
 
 type Weather struct {}
@@ -21,7 +21,7 @@ func (w Weather) Matches() []util.Regex {
 }
 
 func (w Weather) Description() string {
-  return "provides current weather and weather forcasts through the darksky weather api"
+  return "provides current weather and weather forcasts through the darksky weather api.\n you can use the 'remember' command to give jarvis your zipcode and it will use it if you don't provide a zipcode in the command."
 }
 
 func (w Weather) Format() string {
@@ -37,19 +37,43 @@ func (w Weather) OtherDocs() []util.HelpTopic {
 }
 
 func (w Weather) Execute(m util.IncomingSlackMessage) {
-  zipCodeRegex := regexp.MustCompile("[0-9]{5}")
-  zipCode := string(zipCodeRegex.Find([]byte(m.Text)))
-  if zipCode == "" {
-    ws.SendMessage("You should probably provide a zipcode.", m.Channel)
+  // Different types of requests to the weather api match different regexes
+  noZipCodeProvided := util.NewRegex("^jarvis weather$")
+  zipCodeProvided := util.NewRegex("^jarvis weather ([0-9]{5})$")
+  if noZipCodeProvided.Matches(m.Text) {
+    w.noZipCodeProvided(m)
+  } else if zipCodeProvided.Matches(m.Text) {
+    w.zipCodeProvided(zipCodeProvided.SubExpression(m.Text, 0), m)
   } else {
-    weather, err := service.Weather{}.ForecastFriendly(zipCode)
-    switch err.(type) {
-    case service.BadZipCodeError:
-      ws.SendMessage("It doesn't look like the zipcode you provided is valid.", m.Channel)
-    case error:
-      ws.SendMessage("I've encountered an error while getting your weather. Please call my daddy he can help us.", m.Channel)
-    default:
-      ws.SendMessage(weather, m.Channel)
-    }
+    ws.SendMessage("I don't recognize that type of weather request. Sorry :(", m.Channel)
+  }
+}
+
+func (w Weather) noZipCodeProvided(m util.IncomingSlackMessage) {
+  in, storedZipCode := data.GetDatum("my zip", m.User)
+  if !in {
+    ws.SendMessage("You didn't provide a location and I don't have one on file for you. Bummer.", m.Channel)
+    return
+  }
+  weather, err := service.Weather{}.ForecastFriendly(storedZipCode)
+  switch err.(type) {
+  case service.BadZipCodeError:
+    ws.SendMessage("The zip code I have on file for you doesn't appear to be valid. Might want to fix that.", m.Channel)
+  case error:
+    ws.SendMessage("A fatal error has occured. Don't worry I'm fine.", m.Channel)
+  default:
+    ws.SendMessage("Here's my forecast for " + storedZipCode + ":\n" + weather, m.Channel)
+  }
+}
+
+func (w Weather) zipCodeProvided(zipcode string, m util.IncomingSlackMessage) {
+  weather, err := service.Weather{}.ForecastFriendly(zipcode)
+  switch err.(type) {
+  case service.BadZipCodeError:
+    ws.SendMessage("It doesn't look like the zipcode you provided is valid. Bummer.", m.Channel)
+  case error:
+    ws.SendMessage("A really bad error occured during my attempt to predict the future. Of weather. Sorry.", m.Channel)
+  default:
+    ws.SendMessage(weather, m.Channel)
   }
 }
