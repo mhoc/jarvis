86a56d7ae6a9ee8c5a0eca967bbfc985add2093f
Mike Hockerman <mike@hockerman.com>
2015-12-14 15:44:55 -0500

    Check for maximum message length

--- src/jarvis/ws/writer.go
+++ src/jarvis/ws/writer.go
@@ -16,6 +16,10 @@ func SendMessage(message string, channelId string) {
   } else {
     log.Trace(fmt.Sprintf("Writing message '%v' to channel '%v'", message, channelId))
   }
+  if len(message) > 1000 {
+    log.Trace("Attempting to send message that is far too large")
+    message = "The message I just attempted to send you exceed Slack's maximum message length."
+  }
   msg := util.OutgoingSlackMessage{
     Channel: channelId,
     Text: message,
