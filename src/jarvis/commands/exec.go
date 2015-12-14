a37bed42f33bf0aba8fcd202aad605a296956ef8
Mike Hockerman <mike@hockerman.com>
2015-12-14 15:20:44 -0500

    Rudimentary exec sh

--- src/jarvis/commands/exec.go
+++ src/jarvis/commands/exec.go
@@ -35,8 +35,35 @@ func (c Exec) OtherDocs() []util.HelpTopic {
 
 func (c Exec) SubCommands() []util.SubCommand {
   return []util.SubCommand{
+    util.NewSubCommand("^jarvis exec sh (?P<command>.+)$", c.Sh),
     util.NewSubCommand("^jarvis exec python (?P<command>.+)$", c.Python),
   }
+}
+
+func (c Exec) Sh(m util.IncomingSlackMessage, r util.Regex) {
+  command := r.SubExpression(m.Text, 0)
+  result, err := service.Docker{}.RunShInContainer(command, 10 * time.Second)
+  if len(result) > 0 {
+    if result[len(result)-1] == '\n' {
+      result = result[:len(result)-1]
+    }
+  }
+  var msg string
+  if result != "" {
+    msg = "Result: \n```"
+    msg += result + "\n```"
+    if err != nil {
+      msg += "\n"
+    }
+  }
+  if err != nil {
+    msg = "Your command resulted in an error:\n```"
+    msg += err.Error() + "\n```"
+  }
+  if len(msg) == 0 {
+    msg = "Your command generated no output."
+  }
+  ws.SendMessage(msg, m.Channel)
 }
 
 func (c Exec) Python(m util.IncomingSlackMessage, r util.Regex) {
--- src/jarvis/service/docker_service.go
+++ src/jarvis/service/docker_service.go
@@ -16,6 +16,32 @@ type CommandResult struct {
 
 type Docker struct {}
 
+func (d Docker) RunShInContainer(command string, timeout time.Duration) (string, error) {
+  log.Info("Executing command '%v' in container 'ubuntu'", command)
+  resultCh := make(chan CommandResult)
+  cmd := exec.Command("docker", "run", "ubuntu", command)
+  go func() {
+    out, err := cmd.Output()
+    log.Trace("Result: %v", string(out))
+    if err != nil {
+      log.Trace("Error: %v", err.Error())
+    } else {
+      log.Trace("No error")
+    }
+    resultCh <- CommandResult{
+      Text: string(out),
+      Error: err,
+    }
+  }()
+  select {
+  case res := <-resultCh:
+    return res.Text, res.Error
+  case <-time.After(timeout):
+    cmd.Process.Kill()
+    return "", fmt.Errorf("Your command took longer than %v to run and has thus been killed.", Time{}.DurationToString(timeout))
+  }
+}
+
 func (d Docker) RunPythonInContainer(command string, timeout time.Duration) (string, error) {
   log.Info("Executing command '%v' in container 'python'", command)
   resultCh := make(chan CommandResult)
